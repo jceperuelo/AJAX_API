b01e14a6ccd11c5211e89dcbd9465ec8861133e7
tonyponyy <58556009+tonyponyy@users.noreply.github.com>
2021-06-03 19:26:54 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,10 +8,10 @@ var recu = (num) => {
     success: function (respuesta) {
       valor = respuesta;
       $("#imagen" + num).attr("src", respuesta.image);
-      $("#titulo" + num).html(respuesta.name);
-      $("#especie" + num).html(respuesta.species);
-      $("#tipo" + num).html(respuesta.type);
-      $("#genero" + num).html(respuesta.gender);
+      $("#titulo" + num).html("<b>name :</b>" + respuesta.name);
+      $("#especie" + num).html("<b>specie:</b>" + respuesta.species);
+      $("#tipo" + num).html("<b>type:</b>" + respuesta.type);
+      $("#genero" + num).html("<b>Gender :</b>" + respuesta.gender);
     },
     error: function () {
       console.log("No se ha podido obtener la información");
