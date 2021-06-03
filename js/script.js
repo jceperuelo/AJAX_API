fe26ecdff375050854687f4d08d0e7ddc1a726f3
tonyponyy <58556009+tonyponyy@users.noreply.github.com>
2021-06-03 18:58:18 +0200

    codigo js

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var recu = (num) => {
+  let alea =
+    "https://rickandmortyapi.com/api/character/" +
+    Math.trunc(Math.random() * (672 - 1) + 1);
+
+  $.ajax({
+    url: alea,
+    success: function (respuesta) {
+      $("#imagen" + num).attr("src", respuesta.image);
+      $("#titulo" + num).html(respuesta.name);
+      $("#especie" + num).html(respuesta.especies);
+      $("#tipo" + num).html(respuesta.type);
+      $("#genero" + num).html(respuesta.gender);
+    },
+    error: function () {
+      console.log("No se ha podido obtener la información");
+    },
+  });
+};
+
+window.onload = function () {
+  for (let i = 0; i < 6; i++) {
+    recu(i);
+  }
+};
